d834f9450990943f52c75928d7353c9663bbe4ec
TranTuanMinh-rmit <s3806812@rmit.edu.vn>
2023-03-31 20:44:59 +0700

    Added functions for Adding and Deleting a User data entry into the DB
    For more information of my implementation. It is base on this video:
    [https://www.youtube.com/watch?v=s1frrNxq4js]

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 // Import the functions we need from the SDKs
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from "firebase/firestore";
 
 // Initialize Firebase
 /// Init Firebase app
@@ -9,8 +9,10 @@ const app = initializeApp(firebaseConfig);
 
 /// Init Firestore Service
 const db = getFirestore()
+
 /// Collection Ref
 const colRef = collection(db, 'Users');
+
 /// Get Collection Data
 getDocs(colRef)
     .then((snapshot) => {
@@ -24,6 +26,35 @@ getDocs(colRef)
         console.log(err.message)
     })
 
+/// Adding a User. This is use with a form.
+/// All form when regarding the adding of a user with the field Username and Password, ALWAYS use Username & Password. DO NOT use the lowercase
+const addUserForm = document.querySelector('.add')
+addUserForm.addEventListener('submit', (e) => {
+    e.preventDefault
+
+    addDoc(colRef, {
+        Username: addUserForm.Username.value,
+        Password: addUserForm.Password.value,
+    })
+    .then(() => {
+        addUserForm.reset()
+    })
+})
+
+/// Deleting a User. This is use with a form
+/// All form when regarding the adding of a user with the field Username and Password, ALWAYS use Username & Password. DO NOT use the lowercase
+const deletingUserForm = document.querySelector('.delete')
+deletingUserForm.addEventListener('submit', (e) => {
+    e.preventDefault
+
+    const docRef = doc(db, 'Users', deletingUserForm.id.value)
+
+    deleteDoc(docRef)
+        .then(() => {
+            deletingUserForm.reset()
+        })
+
+})
 
 /// Init Analytics
 ///const analytics = getAnalytics(app);
