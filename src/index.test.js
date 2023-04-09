760cd383ced6f984697f2441d70efbbbb924b7d3
TranTuanMinh-rmit <s3806812@rmit.edu.vn>
2023-04-09 17:00:36 +0700

    Added SignUp/In/Out.
    The previous function of creating a User is not the intended way. However, it still has it use.
    
    Will be looking into proper User DB entry intergration with Firebase Auth.

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,8 @@
 // Import the functions we need from the SDKs
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { getFirestore, collection, getDoc, addDoc, deleteDoc, doc, updateDoc, onSnapshot, query, where, serverTimestamp, createUserWithEmailAndPassword} from "firebase/firestore";
+import { getAuth, signInWithPopup, signOut, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
 
 // Initialize Firebase
 /// Init Firebase app
@@ -9,41 +10,100 @@ const app = initializeApp(firebaseConfig);
 
 /// Init Firestore Service
 const db = getFirestore()
+const auth = getAuth()
 
 /// Collection Ref
 const colRef = collection(db, 'Users');
 
+const q = query(colRef, orderBy('createdAt'))
+
 /// Get Collection Data
-getDocs(colRef)
-    .then((snapshot) => {
-        let Users = []
-        snapshot.docs.forEach((doc) => {
-            Users.push({ ...doc.data(), id: doc.id })
-        })
-        console.log(Users)
+/// Real time Collection data
+onSnapshot(colRef, (snapshot) => {
+    let Users = []
+    snapshot.docs.forEach((doc) => {
+        Users.push({ ...doc.data(), id: doc.id })
     })
-    .catch(err => {
-        console.log(err.message)
-    })
+    console.log(Users)
+})
 
 /// Adding a User. This is use with a form.
-/// All form when regarding the adding of a user with the field Username and Password, ALWAYS use Username & Password. DO NOT use the lowercase
-const addUserForm = document.querySelector('.add')
-addUserForm.addEventListener('submit', (e) => {
+/// All form when regarding the adding of a collectiong with the field, HTML and .js ALWAYS have to match the one on the Firebase DB
+
+/// To be revisted and repurposed
+const addColForm = document.querySelector('.add')  /// .add refers to the class of the form in html. Col stands for Collection
+addColForm.addEventListener('submit', (e) => {     /// submit 
     e.preventDefault
 
     addDoc(colRef, {
         Username: addUserForm.Username.value,
         Password: addUserForm.Password.value,
+        createdAt: serverTimestamp(),
     })
     .then(() => {
         addUserForm.reset()
     })
 })
 
+/// Signing up a new User
+const signUpForm = document.querySelector('.signup')
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = signUpForm.email.value        /// The .email refers to the type in <input type="email" name="email"> of HTML
+    const password = signUpForm.password.value  /// Same thing as above
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console/log('User created:', cred.user)
+            signUpForm.reset()
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+/// Logging in and out
+/// Logging out is coded for the purpose of using a button in mind 
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      //console.log('User Signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      //console.log('User Logged in:', cred.user)
+      loginForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+/// Subscribing to Auth Changes
+onAuthStateChanged(auth, (user) => {
+    console.log('user status changed:', user)
+})
+
+
 /// Deleting a User. This is use with a form
-/// All form when regarding the adding of a user with the field Username and Password, ALWAYS use Username & Password. DO NOT use the lowercase
-const deletingUserForm = document.querySelector('.delete')
+/// All form when regarding the adding of a collectiong with the field, HTML and .js ALWAYS have to match the one on the Firebase DB
+
+/// To be revisted
+const deletingUserForm = document.querySelector('.delete')  /// .delete refers to the class of the form in html
 deletingUserForm.addEventListener('submit', (e) => {
     e.preventDefault
 
@@ -56,9 +116,32 @@ deletingUserForm.addEventListener('submit', (e) => {
 
 })
 
-/// Updating a User data
-/// 
-const updaingUser = doc();
+/// Get a single User data
+const docRef = doc(db, 'User', /**/)
+
+onSnapshot(docRef, (doc) => {
+    console.log(doc.data(), doc.id)
+})
+
+/// Updating a User data using Form
+/// All form when regarding the updating of a user with the field Username and Password, ALWAYS use Username & Password. DO NOT use the lowercase
+const updatingUserForm = document.querySelector('.update')  /// .update refers to the class of the form in html
+updatingUserForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'User', updatingUserForm.id.value)
+    
+    updateDoc(docRef, {
+        Username: 'updated usename'
+    })
+    .then(() => {
+        updatingUserForm.reset
+    })
+
+})
+
+
+
 
 /// Init Analytics
 ///const analytics = getAnalytics(app);
