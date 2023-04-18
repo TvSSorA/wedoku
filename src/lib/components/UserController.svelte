7fb0f397da352dcf310705ad255923ae0ea3d8bd
TranTuanMinh-rmit <s3806812@rmit.edu.vn>
2023-04-18 16:07:22 +0700

    Semi-workable. Modal need styling.

--- src/lib/components/UserController.svelte
+++ src/lib/components/UserController.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
     import { initializeApp } from "firebase/app";
-    import { getFirestore, collection, getDoc, addDoc, deleteDoc, doc, updateDoc, onSnapshot, query, where, serverTimestamp, createUserWithEmailAndPassword, orderBy} from "firebase/firestore";
-    import { getAuth, signInWithPopup, signOut, signInWithEmailAndPassword, onAuthStateChanged, type User } from 'firebase/auth'   
-    
+    import { getFirestore, collection, getDoc, addDoc, deleteDoc, doc, updateDoc, onSnapshot, query, where, serverTimestamp, orderBy} from "firebase/firestore";
+    import { getAuth, signInWithPopup, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, type User } from 'firebase/auth'   
+    import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId } from "$env/static/private";
+
     const firebaseConfig = {
-        apiKey: "AIzaSyAUqY5UKxho8fC9QNI70YlhpQSoe_r2-2c",
-        authDomain: "wedoku-d3500.firebaseapp.com",
-        projectId: "wedoku-d3500",
-        storageBucket: "wedoku-d3500.appspot.com",
-        messagingSenderId: "381287339740",
-        appId: "1:381287339740:web:37d81f7ff25213cd044619",
-        measurementId: "G-MMBM958V09"
-    };  
+        apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId
+    }
 
     // Initialize Firebase
     /// Init Firebase app
@@ -39,40 +34,40 @@
 
     /// Signing up a new User
     const signUpForm = document.querySelector('.modal-signup')
-    signUpForm.addEventListener('submit', (e) => {
+    signUpForm!.addEventListener('submit', (e) => {
         e.preventDefault()
 
-        const email = signUpForm.email.value        /// The .email refers to the type in <input type="email" name="email"> of HTML
-        const password = signUpForm.password.value  /// Same thing as above
+        const email = signUpForm!.email.value       /// The .email refers to the type in <input type="email" name="email"> of HTML
+        const password = signUpForm!.password.value  /// Same thing as above
 
         createUserWithEmailAndPassword(auth, email, password)
             .then((cred) => {
                 return db.collection('Users').doc(cred.user.uid).set({
-                    username: signUpForm['signup-username'].value,        /// The .signup-username refers to the type in <input type="signup-user" name="email"> of HTML I think. Saw this sample from like Firebase 8 or smth. Lfmao
-                    email: signUpForm['email'].value
+                    username: signUpForm!['username'].value,        /// The .signup-username refers to the type in <input type="signup-user" name="email"> of HTML I think. Saw this sample from like Firebase 8 or smth. Lfmao
+                    email: signUpForm!['email'].value
                 })
             })
             .catch((err) => {
                 console.log(err.message)
             }).then(() => {
-                console/log('User created:', cred.user)
-                signUpForm.reset()
+                console.log('User created:', cred.user)
+                signUpForm!.reset()
             })
     })
 
     
     /// Logging in and out
     const loginForm = document.querySelector('.login')
-        loginForm.addEventListener('submit', (e) => {
+        loginForm!.addEventListener('submit', (e) => {
         e.preventDefault()
 
-        const email = loginForm.email.value
-        const password = loginForm.password.value
+        const email = loginForm!.email.value
+        const password = loginForm!.password.value
 
         signInWithEmailAndPassword(auth, email, password)
             .then(cred => {
             //console.log('User Logged in:', cred.user)
-            loginForm.reset()
+            loginForm!.reset()
             })
             .catch(err => {
             console.log(err.message)
