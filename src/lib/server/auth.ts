21243ae4567c998d36fa7c59336ee011f92c60c9
TranTuanMinh-rmit <s3806812@rmit.edu.vn>
2023-05-02 21:13:05 +0700

    Google Pop-up Sign In

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,6 +1,7 @@
 import { env } from "$env/dynamic/private";
-import { initializeApp } from "firebase/app";
+import { FirebaseError, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, signInWithPopup,  } from "firebase/auth";
 
 export function authenticate() {
     const { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } = env;
@@ -10,4 +11,31 @@ export function authenticate() {
     const db = getFirestore(app);
 
     return db
+}
+
+export function signInGoogle() {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider;
+
+    document.getElementById("loginGoogle")?.addEventListener('click', function(){
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential!.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error: FirebaseError) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData?.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    })
 }
